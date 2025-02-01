91066eea47cd0d07aa4af4f3df6b9a12514017fb
Jordan Paul <jordan@getpixeltag.com>
2025-02-01 11:00:18 -0800

    basic services

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
-import { Agent, ZeeWorkflow } from "@covalenthq/ai-agent-sdk";
+import { Agent, ZeeWorkflow, createTool } from "@covalenthq/ai-agent-sdk";
+import z, { type ZodType } from "zod";
 import "dotenv/config";
+
+const tool = createTool({
+  id: "get-company-report",
+  description: "This tool is used to get the current state of the company",
+  schema: z.object({}),
+  execute: async (params) => {
+      return "The current state of the company is good";
+  },
+});
 
 const agent1 = new Agent({
     name: "Agent1",
@@ -8,24 +18,24 @@ const agent1 = new Agent({
         name: "gpt-4o-mini",
     },
     description: "A helpful AI assistant that can engage in conversation.",
+    tools: {
+      'Agent1' : tool,
+    }
 });
 
-const agent2 = new Agent({
-    name: "Agent2",
-    model: {
-        provider: "OPEN_AI",
-        name: "gpt-4o-mini",
-    },
-    description: "A helpful AI assistant that can engage in conversation.",
-});
 
 const zee = new ZeeWorkflow({
     description: "A workflow of agents that do stuff together",
     output: "Just bunch of stuff",
-    agents: { agent1, agent2 },
+    agents: { agent1 },
 });
 
 (async function main() {
-    const result = await zee.run();
+    const result = await agent1.run({
+      agent: "",
+      messages: [],
+      status: "idle",
+      children: []
+    });
     console.log(result);
 })();
