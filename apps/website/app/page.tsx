5a4c57f4b212c415b05cbb7b348a4dec1bcd5202
Jordan Paul <jordan@getpixeltag.com>
2025-02-08 19:37:13 -0800

    chain match

--- apps/website/app/page.tsx
+++ apps/website/app/page.tsx
@@ -5,12 +5,13 @@ import { useAccount } from "wagmi";
 import ETFBuilder from "@/components/etfBuilder";
 
 export default function Home() {
-  const {isConnected} = useAccount();
+  const {isConnected, chainId} = useAccount();
   
   return (
     <Stack direction="column" spacing={2} justifyContent="center" alignItems="center" mt={8}>
       {!isConnected && <Typography variant="h4" pt={8}>Connect your wallet</Typography>}
-      {isConnected && <ETFBuilder />}
+      {isConnected && chainId !== 137 && <Typography variant="h4" pt={8}>Switch to Polygon Mainnet</Typography>}
+      {isConnected && chainId === 137 && <ETFBuilder />}
     </Stack>
   );
 }
